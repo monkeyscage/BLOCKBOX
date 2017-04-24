296c9d5445c0521ff79f4185faaeed0c395e353f
maxxflyer <maxxflyer@gmail.com>
2017-04-24 23:55:25 +0200

    Update generator.sol

--- generator.sol
+++ generator.sol
@@ -88,13 +88,15 @@ function addString(address d,address addr,uint index,string info) returns(bool){
 contract universalBox{
 address public owner; //standard needed for Alpha Layer and generic augmentation
 address public controller; //allowed to post, share and edit logs
-string standard="BLOCKLOG.1.0";  //the blog standard
+string standard="BLOCKBOX.1.0";  //the blog standard
 uint public logcount;
+uint public totExposed;
  
 //creation
 function universalBox(address o) {
 owner=o;
 logcount=1;
+totExposed=1;
 logs.push(log(o,block.number,0,1));
 }
 
@@ -113,43 +115,30 @@ return true;
 }
  
 //add a new post at the end of the log
-function shareLog(address ethlink) returns(bool){
+function addEntity(address ethEntity) returns(bool){
 if((msg.sender!=owner)&&(msg.sender!=controller))throw;
-logs.push(log(ethlink,block.number,logcount-1,logcount+1));
+logs.push(log(ethEntity,block.number,logcount-1,logcount+1));
 logcount++;
-return true;
-}
- 
-//add a new post at the end of the log
-function createLog() returns(bool){
-if((msg.sender!=owner)&&(msg.sender!=controller))throw;
-address a=new microlog(msg.sender);
-logs.push(log(a,block.number,logcount-1,logcount+1));
-logcount++;
-return true;
-}
- 
-//edit a specific post at a given index
-function editPost(uint index,address ethlink) returns(bool){
-if((msg.sender!=owner)&&(msg.sender!=controller))throw;
-logs[index]=log(ethlink,block.number,logcount-1,logcount+1);
-logcount++;
+totExposed++;
 return true;
 }
 
+
+
 //delete a specific post at a given index
-function deletePost(uint index,address ethlink) returns(bool){
+function deleteEntity(uint index) returns(bool){
 if((msg.sender!=owner)&&(msg.sender!=controller))throw;
 log l=logs[index];
 logs[l.prev].next=l.next;
 logs[l.next].prev=l.prev;
+totExposed--;
 return true;
 }
  
 //read the logs by index
 function readLog(uint i)constant returns(uint,address,uint,uint,uint,uint){
 log l=logs[i];
-return(logs.length,l.ethlink,l.blocknumber,l.prev,l.next,logcount);
+return(logs.length,l.ethlink,l.blocknumber,l.prev,l.next,totExposed);
 }
 
 //the logs container
@@ -165,7 +154,7 @@ string public logInterface="a-Log|u-Block|u-PrevLog|u-NextLog|u-TotExposed";
    }
  
  
-//destroy blog
+//destroy box
 function kill(){
 if (msg.sender != owner)throw;
 selfdestruct(owner);
